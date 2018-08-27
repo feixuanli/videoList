099db71ab7c30454a494cf737e859a5c36790da7
Feixuan Li <feixuanl@usc.edu>
2018-08-26 20:47:56 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-// create a new component,
-// this component should produce some html 
-const App = () => {
-    return <div>hi !</div>;
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDOIZR6pY1afFJl2TUpQrNMmHmoqqUgz-k';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos:[], selectedVideo: null};
+
+        YTSearch({key: API_KEY, term:'surfboards'}, videos => {
+            this.setState({ videos: videos, selectedVideo: videos[0] });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
-// take this components generated html and put it on the page in the dom 
+
+
+ReactDOM.render(<App />, 
+    document.querySelector('.container'));
